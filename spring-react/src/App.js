98d9abb64edd0599593e6b60ea99014b3bda8c8d
kavir <kavirmansingh7@gmail.com>
2025-12-20 11:44:34 +0545

    push before laptop-pc

--- spring-react/src/App.js
+++ spring-react/src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to the home page</h1>
+    <div className="bg-slate-800 h-16 px-16 items-center py-4 flex ">
+     <h1 className="text-3xl font-bold  ">E-Mhicha</h1>
+     <div className='space-x-4 ml-auto'>
+     <a href="/" className='hover:text-gray-400'>Home</a>
+     <a href="/"className='hover:text-gray-400'>Profile</a>
+     <a href="/" className='hover:text-gray-400'>Contact</a>
+     </div>
+   
     </div>
   );
 }
